2238cc794733d7aae99a7f1408710bb75a2a564c
dfergusbrown <dfergusbrown@gmail.com>
2023-09-02 12:46:38 -0400

    then to await conversion

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -233,15 +233,7 @@ const Main = () => {
     }, [dispatch])
     
     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) => {
-            Platform.OS === 'ios'
-                ? Alert.Alert('Initial Network Connectivity Type:', connectionInfo.type)
-                : ToastAndroid.show(
-                    'Initial Network Connectivity Type: ' + 
-                        connectionInfo.type, 
-                    ToastAndroid.LONG
-                )
-        })
+        showNetInfo();
 
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
@@ -251,6 +243,19 @@ const Main = () => {
 
         return unsubscribeNetInfo;
     }, [])
+
+    const showNetInfo = async () => {
+        const connectionInfo = await NetInfo.fetch()
+        
+        Platform.OS === 'ios'
+            ? Alert.Alert('Initial Network Connectivity Type:', connectionInfo.type)
+            : ToastAndroid.show(
+                'Initial Network Connectivity Type: ' + 
+                    connectionInfo.type, 
+                ToastAndroid.LONG
+            )
+        
+    }
 
     const handleConnectivityChange = (connectionInfo) => {
         let connectionMsg = 'You are now connected to an active network.';
